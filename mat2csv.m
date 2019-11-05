59fab656de9f4f3f08450f900c7b8ebb32f025d4
Connor Lee <connorlee77@gmail.com>
2019-11-04 22:21:44 -0800

    first commit

---
+++ mat2csv.m
@@ -0,0 +1,53 @@
+files = dir('data/');
+files = files(3:end);
+
+% discrete timesteps
+dt = 0.01;
+
+% u parameters
+m = 17;
+L = 0.1;
+I = 20;
+b = 0.1;
+
+u = [-1/m, -1/m, 0, 0, 1/m, 1/m, 0, 0;
+    0, 0, -1/m -1/m, 0, 0, 1/m, 1/m;
+    -L/(2*I), L/(2*I), -b/(2*I), b/(2*I), -L/(2*I), L/(2*I), -b/(2*I), b/(2*I)];
+
+B = @(theta) rotz(rad2deg(theta))';
+
+% first block matrix
+A1 = zeros(6);
+A1(1:3, 4:6) = eye(3);
+
+for k=1:length(files)
+    data = load(join(['C:\Users\conno\Dropbox\caltech\g2\fall\acm154\project\data\', files(k).name]));
+    data = get(data.statedata, 'Data');
+    
+    num = files(k).name(2:end-4);
+    
+    F = zeros(8,1);
+    for j=1:length(num)
+        F(str2num(num(j))) = 1;
+    end
+    
+    phi_F = zeros(length(data), 11);
+    for i=2:length(data)
+        
+        theta_t1 = data(i-1,3);
+        v_t1 = data(i-1,1:6)';
+        
+        v_t2 = data(i,1:6)';
+        
+        % Last block matrix
+        A3 = zeros(6,3);
+        A3(4:6,:) = B(theta_t1);
+        
+        % discretized dynamics
+        phi_mat = (v_t2 - (eye(6) + A1*dt)*v_t1 - dt*A3*u*F)/dt;
+        phi_F(i-1,:) = [phi_mat(4:6)', F'];
+    end
+    data = horzcat(data, phi_F);
+    data = data(1:end-1,:);
+    csvwrite(join(['csv/', files(k).name(1:end-3), 'csv']), data);
+end
---
+++ test.py
@@ -0,0 +1,111 @@
+import numpy as np
+import os
+import torch
+import torch.optim as optim
+import natsort 
+import tqdm 
+import torch.utils.data as data_utils
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+DATA_PATH = 'csv'
+
+data = []
+files = os.listdir(DATA_PATH)
+files = natsort.natsorted(files)
+for file in files:
+	filepath = os.path.join(DATA_PATH, file)
+	print(file)
+	f = np.loadtxt(filepath, delimiter=',')
+	data.append(f)
+data = np.vstack(data)
+
+xdata = np.zeros((len(data), 17))
+xdata[:,:9] = data[:,:9]
+xdata[:,9:] = data[:,12:]
+
+ydata = data[:,9:12]
+x_train, x_test, y_train, y_test = xdata[:7000,:], xdata[7000:,:], ydata[:7000,:], ydata[7000:,:]
+print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+batch_size, D_in, H, D_out = 64, 17, 5, 3
+
+# Create random Tensors to hold inputs and outputs
+tensor_data = torch.from_numpy(x_train).float()
+tensor_target = torch.from_numpy(y_train).float()
+
+tensor_data_val = torch.from_numpy(x_test).float()
+tensor_target_val = torch.from_numpy(y_test).float()
+
+train_dataset = data_utils.TensorDataset(tensor_data, tensor_target)
+train_loader = data_utils.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = data_utils.TensorDataset(tensor_data_val, tensor_target_val)
+test_loader = data_utils.DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+# Use the nn package to define our model as a sequence of layers. nn.Sequential
+# is a Module which contains other Modules, and applies them in sequence to
+# produce its output. Each Linear Module computes output from input using a
+# linear function, and holds internal Tensors for its weight and bias.
+model = torch.nn.Sequential(
+	torch.nn.Linear(D_in, H),
+	torch.nn.ReLU(),
+	torch.nn.Linear(H, D_out),
+)
+
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+# The nn package also contains definitions of popular loss functions; in this
+# case we will use Mean Squared Error (MSE) as our loss function.
+loss_fn = torch.nn.MSELoss(reduction='sum')
+
+test_losses = []
+train_losses = []
+epochs = 300
+for t in range(epochs):
+
+	train_loss = 0
+	for batch_index, (x, y) in enumerate(train_loader):
+		
+		# Forward pass: compute predicted y by passing x to the model.
+		y_pred = model(x)
+
+		# Compute and print loss.
+		loss = loss_fn(y_pred, y)
+		train_loss += loss.item() / batch_size
+
+		# Zero the gradients before running the backward pass.
+		model.zero_grad()
+
+		# Backward pass: compute gradient of the loss with respect to all the learnable
+		# parameters of the model. Internally, the parameters of each Module are stored
+		# in Tensors with requires_grad=True, so this call will compute gradients for
+		# all learnable parameters in the model.
+		loss.backward()
+
+		# Update the weights using gradient descent. Each parameter is a Tensor, so
+		# we can access its gradients like we did before.
+		with torch.no_grad():
+			optimizer.step()
+			optimizer.zero_grad()
+
+	with torch.no_grad():
+		test_loss = 0
+		for x, y in test_loader:
+			y_pred = model(x)
+			test_loss += loss_fn(y_pred, y).item()
+
+		print("Epoch: {} | Training loss: {}".format(t, train_loss / len(train_loader)))
+		print("Epoch: {} | Testing loss: {}".format(t, test_loss / len(test_loader)))
+		train_losses.append(train_loss / len(train_loader))
+		test_losses.append(test_loss / len(test_loader))
+
+plt.plot(range(1,epochs), train_losses[1:])
+plt.plot(range(1,epochs), test_losses[1:])
+plt.legend(['Train', 'Test'])
+plt.title('MSE Loss ')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.savefig('losses.png')
